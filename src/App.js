b094bab928ddfbedc6c58e36e782ce64c4bdc392
Chaim Finkelman <chaim@Chaims-MBP.attlocal.net>
2019-03-02 18:09:45 -0800

    change defualt title

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <TodoList title="Boring Programming List" />
+        <TodoList title="First To Do List" />
       </div>
     );
   }
