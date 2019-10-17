de1a4f03034cf760a11b855cf67077d6a051b810
Chaim Finkelman <toChaim@gmail.com>
2019-10-16 18:32:36 -0700

    fix todoList

--- src/TodoList.js
+++ src/TodoList.js
@@ -6,22 +6,19 @@ import EditTodoForm from "./EditTodoForm";
 class TodoList extends Component {
 	constructor(props) {
 		super(props);
-		let storedState = JSON.parse(localStorage.getItem("todoState"));
-		if (storedState) {
-			this.state = storedState;
-		} else
-			this.state = {
-				idCnt: 2,
-				todos: [
-					{ name: "Have fun programming", id: 1, complete: false },
-					{ name: "Take a nap.", id: 2, complete: false }
-				]
-			};
+		this.state = JSON.parse(localStorage.getItem("todoState")) || {
+			idCnt: 2,
+			todos: [
+				{ name: "Have fun programming", id: 1, complete: false },
+				{ name: "Take a nap.", id: 2, complete: false }
+			]
+		}
 		this.handleComplete = this.handleComplete.bind(this);
 		this.handleAdd = this.handleAdd.bind(this);
 		this.handleEdit = this.handleEdit.bind(this);
 		this.handleSave = this.handleSave.bind(this);
 		this.handleDelete = this.handleDelete.bind(this);
+		this.handleDeleteList = this.handleDeleteList.bind(this);
 	}
 
 	handleComplete(e) {
@@ -45,10 +42,12 @@ class TodoList extends Component {
 	handleAdd(e) {
 		e.preventDefault();
 		//console.log(e.target.todo.value);
-		var newState = this.state.todos.concat([
-			{ name: e.target.todo.value, id: ++this.state.idCnt }
-		]);
-		this.setState({ todos: newState }, () => {
+		this.setState(
+			{ 
+				todos: this.state.todos.concat([{ name: e.target.todo.value, id: this.state.idCnt + 1 }]), 
+				idCnt: this.state.idCnt + 1 
+			}, 
+				() => {
 			localStorage.setItem("todoState", JSON.stringify(this.state));
 		});
 	}
@@ -84,6 +83,12 @@ class TodoList extends Component {
 		this.setState({ todos: newState }, () => {
 			localStorage.setItem("todoState", JSON.stringify(this.state));
 		});
+	}
+
+	handleDeleteList(e) {
+		let newState = {todos:[], idCnt: 0};
+		localStorage.setItem("todoState", JSON.stringify(newState)); 
+		this.setState(newState);
 	}
 
 	handleDelete(e) {
@@ -123,6 +128,7 @@ class TodoList extends Component {
 				<ul className="TodoList">
 					<h1>{this.props.title}</h1>
 					<NewTodoForm handleAdd={this.handleAdd} />
+				<button onClick={this.handleDeleteList}>Delete List</button>
 					{todoItems}
 				</ul>
 		);
